0b5ceb13eac1a92612db409b03d08453e7e1b804
davidkey <davidkey@gmail.com>
2017-03-03 18:56:46 -0500

    converted to console application (no servlet container)

--- src/main/java/com/dak/cursejarbot/CurseJarBotApplication.java
+++ src/main/java/com/dak/cursejarbot/CurseJarBotApplication.java
@@ -1,12 +1,26 @@
 package com.dak.cursejarbot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CurseJarBotApplication {
+import com.dak.cursejarbot.service.BotService;
 
+@SpringBootApplication
+public class CurseJarBotApplication implements CommandLineRunner {
+	
+	@Autowired
+	private BotService botService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CurseJarBotApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) {
+		botService.startBot();
+		
+		// wait here ..?
 	}
 }
